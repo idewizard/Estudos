e12552f1757fefe9f2fb37222c76b33f92e051f1
otacrazy2123 <andre.sousa.tec@gmail.com>
2020-10-07 21:56:03 -0300

    Initial commit

---
+++ src/main/java/br/com/estudo/AppMain.java
@@ -0,0 +1,10 @@
+package br.com.estudo;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/main/java/br/com/estudo/Empregado.java
@@ -0,0 +1,43 @@
+package br.com.estudo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empregado")
+public class Empregado {
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	private int matricula;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+}
